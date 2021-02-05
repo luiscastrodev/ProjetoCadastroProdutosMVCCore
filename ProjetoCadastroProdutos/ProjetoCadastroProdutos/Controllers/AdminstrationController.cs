f38676e3df033ae05283f369034f1c9ff7e61e2c
luis <luiscastrodev@gmail.com>
2021-02-05 11:35:03 -0300

    listagem de roles

--- ProjetoCadastroProdutos/ProjetoCadastroProdutos/Controllers/AdminstrationController.cs
+++ ProjetoCadastroProdutos/ProjetoCadastroProdutos/Controllers/AdminstrationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoCadastroFuncionarios.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,42 @@ namespace ProjetoCadastroFuncionarios.Controllers
         public AdminstrationController(RoleManager<IdentityRole> roleManager)
         {
             this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // precisamos apenas especificar o nome único da role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                // Salva a role na tabela AspNetRole
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Adminstration");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
